e6ddac4b70b1c034497b9a38684e0818c6863be0
tired_ <omaralim06@gmail.com>
2026-04-20 00:01:44 -0700

    feat: implement get_files_info

---
+++ functions/get_files_info.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+
+    is_valid_path = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+
+    print(f"\ntarget directory: {target_dir}")
+    print(f"is_valid_path: {is_valid_path}\n")
+
+    if not Path(target_dir).is_dir():
+        return f'Error: "{target_dir}" is not a directory'
+    if not is_valid_path:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    
+    dir_contents = []
+    with os.scandir(target_dir) as entries:
+        for entry in entries:
+            try:
+                file_size = entry.stat().st_size
+                name = entry.name
+                is_dir = entry.is_dir()
+                dir_contents.append(f"{name}: file_size={file_size} bytes, is_dir={is_dir}")
+            except Exception as e:
+                return f"Error: {e}"
+    return dir_contents
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,19 +15,32 @@ client = genai.Client(api_key = api_key)
 
 parser = argparse.ArgumentParser(description="Kayo Coding Agent")
 parser.add_argument("user_prompt", type=str, help="User input prompt")
+parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
 # Now we can access `args.user_prompt`
 prompt = args.user_prompt
+verbose = args.verbose
 
-def getResponse(content):
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+def getResponse():
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=content
+        model="gemma-4-31b-it", contents=messages
     )
     return response
 
-response = getResponse(prompt)
 
-print(f"Prompt:\n{prompt}")
-print(f"\nInput tokens: {response.usage_metadata.prompt_token_count}")
-print(f"\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-print(f"\nResponse text:\n{response.text}")
+response = getResponse()
+
+if verbose:
+    print(f"Prompt:\n{prompt}")
+    print(f"\nInput tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"\nResponse text:\n{response.text}")
+else:
+    print(response.text)
+
+# list all models available via the gemini api
+# for model in client.models.list():
+#     print(model.name)
